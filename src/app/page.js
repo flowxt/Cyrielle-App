31f06ff9f605b197f7c8503a59c1ab1c9532bb46
florian <florianbarjon@icloud.com>
2025-01-15 12:50:24 +0100

    mise en page

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
           <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
             <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
               <h1 className="text-3xl font-extrabold text-white sm:text-5xl">
-                Créateur d'Expérience
+                Créateurs d'Expériences
                 <strong className="block font-extrabold text-rose-500">
                   {" "}
                   Intéractives{" "}
@@ -47,15 +47,17 @@ export default function Home() {
           alt="fond"
           className="h-96 w-full object-cover object-center"
         /> */}
-        <h3 className="text-3xl bg-gradient-to-b from-gray-300 to-zinc-500 p-16 text-center ">
-          "Libérer le potentiel de vos équipes grâce à des{" "}
-          <span className="text-red-600">expériences</span> sur mesure,
-          <br /> où chaque participant est un acteur clé de la{" "}
-          <span className="text-red-600">réussite collective</span>"
-        </h3>
+        <div className="bg-gradient-to-b from-gray-200 to-zinc-300">
+          <h3 className="md:text-2xl  lg:text-3xl p-16 text-center max-w-3xl mx-auto">
+            "Libérer le potentiel de vos équipes grâce à des{" "}
+            <span className="text-red-600">expériences</span> sur mesure, où
+            chaque participant est un acteur clé de la{" "}
+            <span className="text-red-600">réussite collective</span>"
+          </h3>
+        </div>
       </header>
-      <main>
-        <h2 className="text-4xl bg-gray-200 text-center p-16">
+      <main className="bg-gradient-to-t from-gray-300 to-zinc-500 pt-8 border-t-2 border-gray-900">
+        <h2 className="text-4xl text-center p-16">
           Vos <span className="text-red-600">défis </span>| Nos{" "}
           <span className="text-red-600">solutions</span>{" "}
         </h2>
