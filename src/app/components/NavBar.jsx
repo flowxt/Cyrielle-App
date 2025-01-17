4fec09fc019e8e9762683bb2be8f515f0923f3be
florian <florianbarjon@icloud.com>
2025-01-17 14:24:41 +0100

    text nav

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const NavBar = () => {
   return (
     <header className="bg-neutral-950">
-      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 lg:py-8">
         <div className="flex h-16 items-center justify-between">
           <div className="md:flex md:items-center md:gap-12">
             <a className="block text-teal-600" href="#">
@@ -17,41 +17,41 @@ const NavBar = () => {
             </a>
           </div>
 
-          <div className="hidden md:block">
+          <div className="hidden md:block ">
             <nav aria-label="Global">
-              <ul className="flex items-center gap-6 text-sm">
+              <ul className="flex items-center gap-6 lg:text-lg ">
                 <li>
-                  <Link href="/" className="transition text-white hover:text-white/75">
+                  <Link href="/" className="transition text-white hover:text-red-500/75">
                     Accueil
                   </Link>
                 </li>
                 <li>
-                  <Link href="/team-building" className="transition text-white hover:text-white/75">
+                  <Link href="/team-building" className="transition text-white hover:text-red-500/75">
                     Team Building
                   </Link>
                 </li>
                 <li>
-                  <Link href="/nos-concepts" className="transition text-white hover:text-white/75">
+                  <Link href="/nos-concepts" className="transition text-white hover:text-red-500/75">
                     Nos Concepts
                   </Link>
                 </li>
                 <li>
-                  <Link href="/nos-formations" className="transition text-white hover:text-white/75">
+                  <Link href="/nos-formations" className="transition text-white hover:text-red-500/75">
                     Nos Formations
                   </Link>
                 </li>
                 <li>
-                  <Link href="/agenda" className="transition text-white hover:text-white/75">
+                  <Link href="/agenda" className="transition text-white hover:text-red-500/75">
                     Agenda
                   </Link>
                 </li>
                 <li>
-                  <Link href="/galerie-scenarios" className="transition text-white hover:text-white/75">
+                  <Link href="/galerie-scenarios" className="transition text-white hover:text-red-500/75">
                     Galerie - Scénarios
                   </Link>
                 </li>
                 <li>
-                  <Link href="/blog" className="transition text-white hover:text-white/75">
+                  <Link href="/blog" className="transition text-white hover:text-red-500/75">
                     Blog
                   </Link>
                 </li>
