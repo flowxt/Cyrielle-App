469bf6cae42880df9f353820265c0eb1b638e1e9
florian <florianbarjon@icloud.com>
2025-01-19 17:50:43 +0100

    fleche

--- src/app/components/Carousel.jsx
+++ src/app/components/Carousel.jsx
@@ -13,27 +13,44 @@ export default function Carousel() {
         })
     );
 
-    const [emblaRef] = useEmblaCarousel({ loop: true }, [autoplay.current]);
+    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [autoplay.current]);
 
     // Liste des images
     const images = Array.from({ length: 17 }, (_, i) => `/images/caroussel/img${i + 1}.png`);
 
+    const scrollPrev = () => emblaApi && emblaApi.scrollPrev();
+    const scrollNext = () => emblaApi && emblaApi.scrollNext();
+
     return (
-        <div className="embla overflow-hidden" ref={emblaRef}>
-            <div className="embla__container flex gap-x-4">
-                {images.map((src, index) => (
-                    <div
-                        key={index}
-                        className="embla__slide flex-shrink-0 flex justify-center items-center w-[20%] max-w-[250px] h-[250px] rounded-lg overflow-hidden shadow-lg"
-                    >
-                        <img
-                            src={src}
-                            alt={`Image ${index + 1}`}
-                            className="w-full h-full object-cover"
-                        />
-                    </div>
-                ))}
+        <div className="relative">
+            <div className="embla overflow-hidden p-8" ref={emblaRef}>
+                <div className="embla__container flex gap-x-1">
+                    {images.map((src, index) => (
+                        <div
+                            key={index}
+                            className="embla__slide flex-shrink-0 flex justify-center items-center w-[20%] max-w-[250px] h-[450px] rounded-lg overflow-hidden shadow-2xl"
+                        >
+                            <img
+                                src={src}
+                                alt={`Image ${index + 1}`}
+                                className="w-full h-full object-cover "
+                            />
+                        </div>
+                    ))}
+                </div>
             </div>
+            <button
+                className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-gray-400 text-white p-2 rounded-full ml-2 h-16 w-10"
+                onClick={scrollPrev}
+            >
+                &lt;
+            </button>
+            <button
+                className="absolute top-1/2 right-0 transform -translate-y-1/2 text-white p-2 rounded-full h-16 w-10 bg-gray-400 mr-2"
+                onClick={scrollNext}
+            >
+                &gt;
+            </button>
         </div>
     );
 }
