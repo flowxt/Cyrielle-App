17808254ccf7d75c1823dc09b649925440e707ba
florian <florianbarjon@icloud.com>
2025-01-19 13:33:11 +0100

    test

--- src/app/components/Team.jsx
+++ src/app/components/Team.jsx
@@ -1,3 +1,5 @@
+
+
 export default function Team() {
     return (
         <>
@@ -6,7 +8,7 @@ export default function Team() {
                 <p className="font-roboto text-lg font-light px-8">Un duo complémentaire et passionné à l'écoute de vos besoins, garants d'une expérience unique et immersive.</p>
                 <div className="container flex flex-col lg:flex-row items-center justify-center gap-8 py-8 items-center ">
                 <div className="container-Cyrielle flex flex-col lg:flex-row-reverse items-center gap-8">
-                        <img src='/Cyrielle.jpg' alt="Cyrielle" className="border-2 border-red-700 rounded-xl shadow-2xl shadow-neutral-900 h-80"/>
+                        <img src='/cyrielle.jpg' alt="Cyrielle" className="border-2 border-red-700 rounded-xl shadow-2xl shadow-neutral-900 h-80"/>
                         <div className="content-cyrielle flex flex-col text-center lg:text-end">
                             <h3 className="text-xl font-poppins font-semibold">Cyrielle <span className="text-red-700">Schepens</span></h3>
                             <p className="font-roboto text-lg font-light">
@@ -21,7 +23,7 @@ export default function Team() {
                         </div>
                     </div>
                 <div className="container-Matthieu flex flex-col lg:flex-row items-center gap-8">
-                   <img src="/Matthieu.jpg" alt="Matthieu" className="border-2 border-red-700 rounded-xl shadow-2xl shadow-neutral-900 h-80" />
+                   <img src='/matthieu.jpg' alt="Matthieu" className="border-2 border-red-700 rounded-xl shadow-2xl shadow-neutral-900 h-80" />
                    <div className="content-matthieu flex flex-col text-start">
                    <h3 className="text-xl font-poppins font-semibold">Matthieu <span className="text-red-700">Vernisse</span> </h3>
                <p className="font-roboto text-lg font-light">
