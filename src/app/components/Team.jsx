134aaae7f5f6ce57238f8a2e118d34e8973a2659
florian <florianbarjon@icloud.com>
2025-01-19 12:43:16 +0100

    centre

--- src/app/components/Team.jsx
+++ src/app/components/Team.jsx
@@ -1,18 +1,18 @@
 export default function Team() {
     return (
         <>
-            <div className="container-team text-center py-12">
+            <div className="container-team text-center py-12 flex flex-col items-center gap-8">
                 <h2 className="text-4xl font-poppins font-semibold pb-4">Notre équipe</h2>
                 <p className="font-roboto text-lg font-light">Un duo complémentaire et passionné à l'écoute de vos besoins, garants d'une expérience unique et immersive.</p>
-                <div className="container flex items-center justify-center gap-8 py-8">
+                <div className="container flex items-center justify-center gap-8 py-8 items-center">
                 <div className="container-Cyrielle flex flex-row-reverse items-center gap-8">
                     <img src='/Cyrielle.jpg' alt="Cyrielle" className="border-2 border-red-700 rounded-xl shadow-2xl shadow-neutral-900 h-80"/>
                     <div className="content-cyrielle flex flex-col text-end">
                     <h3 className="text-xl font-poppins font-semibold">Cyrielle <span className="text-red-700">Schepens</span></h3>
                     <p className="font-roboto text-lg font-light">
-                        Facilitatrice en communication par l'improvisation théâtrale
+                        Facilitatrice en communication<br /> par l'improvisation théâtrale
                         <br />
-                        Coordinatrice des équipes - Gestionnaire des projets
+                        Coordinatrice des équipes <br /> Gestionnaire des projets
                         <br />
                         Créatrice de concepts originaux
                         <br />
