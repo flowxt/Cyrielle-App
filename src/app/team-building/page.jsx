6e1348f09941890842a6420c0953a0e84dd233aa
florian <florianbarjon@icloud.com>
2025-01-21 11:13:11 +0100

    carte à faire tb

--- src/app/team-building/page.jsx
+++ src/app/team-building/page.jsx
@@ -29,7 +29,7 @@ export default function TeamBuilding() {
 
       <div className="bg-gradient-to-b from-gray-200 to-gray-300">
         <h3 className="text-2xl md:text-3xl  lg:text-4xl p-6 text-center max-w-3xl mx-auto font-roboto font-semibold">
-        Découvrez nos activités stimulantes et <span className="text-red-600">fédératrice</span>.
+        Découvrez nos activités stimulantes et <span className="text-red-700">fédératrice</span>.
         </h3>
         <p className="md:text-lg lg:text-xl font-roboto text-center max-w-3xl mx-auto px-6">À la recherche de solutions innovantes pour renforcer la cohésion, le partage et l'engagement tout en vivant des expériences inoubliables ?</p>
         <p className="md:text-lg lg:text-xl font-roboto text-center max-w-3xl mx-auto pb-6">Transmettez des valeurs d'équipe en participant à des activités insolites qui favorise la réflexion collective.
@@ -39,11 +39,11 @@ A chaque besoin, un format adapté pour une expérience inédite de team buildin
     </header>
     <main>
       <section>
-        <h2 className="border-b-2 border-red-600 pb-12">Ici mes 3 cartes</h2>
+        <h2 className="border-b-2 border-red-600 bg-gradient-to-b from-gray-200 to-gray-400 pb-12 border-t-2 border-neutral-600">Ici mes 3 cartes</h2>
       </section>
-        <section className="grid grid-cols-1 md:grid-cols-5">
-          <div className="md:col-span-3 bg-gradient-to-t from-gray-200 to-gray-400 px-12 text-center py-6">
-            <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-4 md:pb-12 font-semibold">Réinventons <span className="text-red-600">votre équipe</span></h2>
+        <section className="grid grid-cols-1 bg-gradient-to-t from-gray-200 to-gray-400  md:grid-cols-5 pb-12">
+          <div className="md:col-span-3 px-12 text-center py-6">
+            <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-4 md:pb-12 font-semibold">Réinventons <span className="text-red-700">votre équipe</span></h2>
             <p className="text-lg">
             Chez En Scènes Acting, nous sommes dévoués à <b>transformer votre équipe</b> en une force unifiée, prête à relever tous les <b>défis</b>.
 <br /><br />
@@ -67,8 +67,9 @@ Nous croyons fermement que le succès d'une équipe repose sur la force de ses <
 
 Nous pouvons transformer votre équipe en une <b>véritable force sur la scène professionnelle</b>.
             </p>
+            <button className="mt-12 px-6 py-3 text-white bg-red-800  hover:bg-red-700 mx-auto flex justify-center items-center font-poppins">Créons ensemble votre team building</button>
           </div>
-          <div className="md:col-span-2 grid grid-rows-2 md:gap-4 lg:pt-6">
+          <div className="md:col-span-2 grid grid-rows-2 md:gap-4 lg:pt-6 mr-6">
             <div className="relative w-full h-full hidden md:block">
               <Image
                 src="/tb-img.png"
