d43b6f1b7ffd1ecb52058233f612efef04860a71
florian <florianbarjon@icloud.com>
2025-01-25 17:28:12 +0100

    code anim

--- src/app/components/CardTB.jsx
+++ src/app/components/CardTB.jsx
@@ -1,3 +1,30 @@
+// import React from 'react';
+// import CustomButton from './CustomButton';
+
+// export default function CardTB({ imageSrc, title, content, buttonText }) {
+//   return (
+//     <article className="group flex flex-col h-full shadow-xl rounded-xl overflow-hidden">
+//       <img
+//         alt="image de cohésion d'équipe"
+//         src={imageSrc}
+//         className="h-56 w-full rounded-xl object-cover shadow-xl transition group-hover:grayscale-[50%]"
+//       />
+  
+//       <div className="p-4 flex flex-col flex-grow">
+//         <a href="#">
+//           <h3 className="text-2xl md:text-2xl lg:text-3xl font-medium text-red-700 font-poppins pb-4">{title}</h3>
+//         </a>
+  
+//         <p className="text-xl mt-2 text-sm/relaxed text-gray-500 flex-grow font-roboto font-light" dangerouslySetInnerHTML={{ __html: content }} />
+        
+//         <CustomButton text={buttonText} additionalClasses="mt-4 self-start" />
+//       </div>
+//     </article>
+//   );
+// }
+
+
+// Code pour animer les cartes 
 import React from 'react';
 import { motion } from 'framer-motion';
 import CustomButton from './CustomButton';
--- src/app/components/CardTBComplete.jsx
+++ src/app/components/CardTBComplete.jsx
@@ -1,3 +1,50 @@
+// import React from 'react';
+// import { motion } from 'framer-motion';
+// import CardTB from './CardTB';
+
+// const cardData = [
+//   {
+//     imageSrc: '/images/caroussel/img1.png',
+//     title: 'Fédérer',
+//     content: "Vous souhaitez renformer l'esprit d'équipe de vos collaborateurs à travers une expérience unique.<br /><br /> Le temps de quelques heures, dans un lieu insolite, nous vous transporterons dans nos univers",
+//     buttonText: 'Réussir ensemble'
+//   },
+//   {
+//     imageSrc: '/images/caroussel/img2.png',
+//     title: 'Intégrer',
+//     content: 'Pas toujours évident de trouver sa place dans une équipe déjà bien installée.<br /><br /> Sortez vos collaborateurs du cadre professionnel et permettez leur de reconnaître les talents de chacun pour les valoriser et atteindre des objectifs communs.',
+//     buttonText: 'Révéler votre équipe'
+//   },
+//   {
+//     imageSrc: '/images/caroussel/img3.png',
+//     title: 'Développer',
+//     content: 'Vous êtes convaincu que le format «Réunion» n’est pas le plus efficace pour developper un projet.<br /><br /> A votre écoute pour comprendre vos attentes, nous concevrons un scénario personnalisé pour permettre à vos collaborateurs de réfléchir autrement et d’aboutir à des actions concrètes.',
+//     buttonText: 'Libérer la reflexion'
+//   },
+//   // Ajoutez plus de cartes ici
+// ];
+
+// const CardTBComplete = () => {
+//   return (
+//     <section className="py-8 px-4">
+//     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+//       {cardData.map((card, index) => (
+//         <CardTB
+//           key={index}
+//           imageSrc={card.imageSrc}
+//           title={card.title}
+//           content={card.content}
+//           buttonText={card.buttonText}
+//         />
+//       ))}
+//     </div>
+//   </section>
+//   );
+// };
+
+// export default CardTBComplete;
+
+// Code pour animer les cartes
 import React from 'react';
 import { motion } from 'framer-motion';
 import CardTB from './CardTB';
