5652dfc752e90773b8fe768ef5b6915f5172e8da
florian <florianbarjon@icloud.com>
2025-02-05 14:27:51 +0100

    api

--- src/app/components/GoogleReviews.jsx
+++ src/app/components/GoogleReviews.jsx
@@ -59,4 +59,79 @@ const GoogleReviews = () => {
   );
 };
 
+<<<<<<< HEAD
 export default GoogleReviews;
+=======
+export default GoogleReviews;
+
+
+// 'use client'; // Marque le composant comme côté client
+
+// import { useState, useEffect } from 'react';
+
+// const GoogleReviews = () => {
+//   const [reviews, setReviews] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     const fetchReviews = async () => {
+//       try {
+//         const response = await fetch('https://places.googleapis.com/v1/places/ChIJj_8jHo12DWARSMt8YL8SQ4A?fields=reviews&key=ICI API KEY');
+//         const data = await response.json();
+//         if (data.reviews) {
+//           setReviews(data.reviews);
+//         } else {
+//           throw new Error('Aucun avis trouvé');
+//         }
+//       } catch (err) {
+//         setError(err.message);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchReviews();
+//   }, []);
+
+//   if (loading) {
+//     return <div className="text-center p-4">Chargement des avis...</div>;
+//   }
+
+//   if (error) {
+//     return <div className="text-center p-4 text-red-500">{error}</div>;
+//   }
+
+//   return (
+//     <div className="max-w-4xl mx-auto p-4">
+//       <h2 className="text-xl font-semibold mb-4">Avis Google</h2>
+//       <div className="space-y-4">
+//         {reviews.map((review, index) => (
+//           <div key={index} className="p-4 border rounded-lg shadow-md hover:bg-gray-100">
+//             <div className="flex items-center mb-2">
+              
+//               <div>
+//                 <p className="font-semibold">{review.authorAttribution.displayName}</p>
+//                 {/* Formater la date de publication */}
+//                 <p className="text-gray-500 text-sm">
+//                   {review.publishTime ? new Date(review.publishTime).toLocaleDateString() : 'Date non disponible'}
+//                 </p>
+//               </div>
+//             </div>
+//             {/* Affichage du texte de l'avis */}
+//             <p className="text-gray-800">
+//               {review.text?.text ? review.text.text : 'Avis non disponible'}
+//             </p>
+//             <div className="flex items-center mt-2">
+//               <span className="text-yellow-500">★</span>
+//               <p className="ml-1">{review.rating}</p>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default GoogleReviews;
+>>>>>>> eaa33ae (api)
