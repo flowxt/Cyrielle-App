7159f70b34a14aa1dc102c469754f634b33e363d
florian <florianbarjon@icloud.com>
2025-02-26 22:44:10 +0100

    ok bg

--- src/app/components/GoogleReviews.jsx
+++ src/app/components/GoogleReviews.jsx
@@ -147,7 +147,7 @@ const reviews = [
 
 const GoogleReviews = () => {
   return (
-    <div className="bg-gray-50 py-12 overflow-hidden">
+    <div className=" py-12 overflow-hidden">
       <h2 className="text-3xl md:text-4xl font-bold text-center mb-24 mt-12 font-poppins">Témoignages de nos <span className='px-4 bg-red-800 text-white '>clients</span></h2>
       <div className="relative w-full overflow-hidden mb-24">
         <div className="animate-scroll whitespace-nowrap ">
